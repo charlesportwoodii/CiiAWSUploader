a54aa89894f5444f941474117d611369f9e45dc5
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-07-26 16:30:56 -0500

    Implementation commit

--- CiiAWSUploader.php
+++ CiiAWSUploader.php
@@ -4,34 +4,67 @@ class CiiAWSUploader
 {
 	private $_filename = null;
 
-	public function __construct($config, $hash=false)
+	private $allowedExtensions = array(
+        'png',
+        'jpeg',
+        'jpg',
+        'gif',
+        'bmp'
+    );
+
+    private $sizeLimit = 10485760;
+
+	public function __construct($config)
 	{
+		$size = $_FILES['file']['size'];
+
+		$pathinfo = pathinfo($_FILES['file']['name']);
+        $filename = $pathinfo['filename'];
+        $ext = $pathinfo['extension'];
+
+        if ($size == 0) 
+            return array('error' => Yii::t('ciims.misc', 'File is empty'));
+        
+        if ($size > $this->sizeLimit) 
+            return array('error' => Yii::t('ciims.misc', 'File is too large'));
+
+        if(!in_array(strtolower($ext), $this->allowedExtensions))
+        {
+            $these = implode(', ', $this->allowedExtensions);
+            return array('error' => Yii::t('ciims.misc', "File has an invalid extension, it should be one of {{these}}.", array('{{these}}' => $these)));
+        }
+
 		// Instantiate an S3 client
 		$client = Aws\S3\S3Client::factory(array(
 		    'key'    => $this->decrypt(Yii::app()->params['CiiMS']['AWS_ACCESS_KEY']),
 		    'secret' => $this->decrypt(Yii::app()->params['CiiMS']['AWS_SECRET_ACCESS_KEY'])
 		));
 
-		$this->_filename =md5($_FILES['file']['name']);
+		$this->_filename = md5(md5(CII_CONFIG).md5($filename) .md5(time()));
 		
-		$result = $client->putObject(array(
-		    'Bucket'     => $config['bucket'],
-		    'Key'        => $_FILES['file']['name'],
-		    'SourceFile' => $_FILES['file']['tmp_name']
-		));
+		try {
+			$result = $client->putObject(array(
+			    'Bucket'     => $config['bucket'],
+			    'Key'        => CII_CONFIG.'/'.$this->_filename.'.'.$ext,
+			    'SourceFile' => $_FILES['file']['tmp_name'],
+			    'ACL'        => 'public-read',
+			    'ContentType' => 'image/'.$ext
+			));
+		} catch (Exception $e) {
+			print_r($e->getMessage());
+			die();
+		}
 
 		print_r($result);
 		die();
 	}
 
-	public function getFileName()
-	{
-		return $this->_filename;
-	}
-
 	private function decrypt($param)
 	{
 		$key = pack('H*', "79A0B95440D6302F5844225240DE85D68300C00EFAA3E117281B494D07681A35");
+		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
 		$ciphertext_dec = base64_decode($param);
 	    $iv_dec = substr($ciphertext_dec, 0, $iv_size);
 	    $ciphertext_dec = substr($ciphertext_dec, $iv_size);
