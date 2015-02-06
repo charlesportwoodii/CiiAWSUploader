70e70fb9df6a3418911ae8d25b5d8987458ddbdd
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-02-06 22:14:25 +0000

    Updating to match new cii core configs

--- CiiAWSUploader.php
+++ CiiAWSUploader.php
@@ -1,62 +1,39 @@
 <?php
 
-class CiiAWSUploader
+/**
+ * Allows CiiMS to upload files to AWS S3 CDN
+ */
+Yii::import('cii.utilities.CiiUploader');
+class CiiAWSUploader extends CiiUploader
 {
-	private $_filename = null;
-
-	private $allowedExtensions = array(
-        'png',
-        'jpeg',
-        'jpg',
-        'gif',
-        'bmp'
-    );
-
-    private $sizeLimit = 10485760;
-
-    private $_config = null;
-
-	public function __construct($config)
-	{
-		$this->_config = $config;
-	}
-
 	public function upload()
 	{
+		$key = defined('CII_CONFIG') ? CII_CONFIG : $_SERVER['SERVER_NAME'];
 
-		$size = $_FILES['file']['size'];
+		$check = $this->verifyFile();
 
-		$pathinfo = pathinfo($_FILES['file']['name']);
-        $filename = $pathinfo['filename'];
-        $ext = $pathinfo['extension'];
-
-        if ($size == 0) 
-            return array('error' => Yii::t('ciims.misc', 'File is empty'));
+        if (isset($check['error']))
+            return $check;
         
-        if ($size > $this->sizeLimit) 
-            return array('error' => Yii::t('ciims.misc', 'File is too large'));
-
-        if(!in_array(strtolower($ext), $this->allowedExtensions))
-        {
-            $these = implode(', ', $this->allowedExtensions);
-            return array('error' => Yii::t('ciims.misc', "File has an invalid extension, it should be one of {{these}}.", array('{{these}}' => $these)));
-        }
+        $filename = $check['success'];
+        $fullFileName = $filename.'.'.$this->file->getExtension();
 
 		// Instantiate an S3 client
 		$client = Aws\S3\S3Client::factory(array(
-		    'key'    => $this->decrypt(Yii::app()->params['CiiMS']['AWS_ACCESS_KEY']),
-		    'secret' => $this->decrypt(Yii::app()->params['CiiMS']['AWS_SECRET_ACCESS_KEY'])
+			'key'    => $this->AWS_ACCESS_KEY,
+			'secret' => $this->AWS_SECRET_ACCESS_KEY
 		));
 
-		$this->_filename = md5(md5(CII_CONFIG).md5($filename) .md5(time()));
-		
+		$factory = new CryptLib\Random\Factory;
+		$file = $factory->getLowStrengthGenerator()->generateString(64);
+
 		try {
 			$result = $client->putObject(array(
-			    'Bucket'     => $this->_config['bucket'],
-			    'Key'        => CII_CONFIG.'/'.$this->_filename.'.'.$ext,
-			    'SourceFile' => $_FILES['file']['tmp_name'],
-			    'ACL'        => 'public-read',
-			    'ContentType' => 'image/'.$ext
+				'Bucket'      => $this->bucket
+				'Key'         => $key.'/'.$file.'.'.$this->file->getExtension(),
+				'SourceFile'  => $this->file->tmp_name,
+				'ACL'         => 'public-read',
+				'ContentType' => 'image/'.$this->file->getExtension()
 			));
 		} catch (Exception $e) {
 			return array(
@@ -64,25 +41,12 @@ class CiiAWSUploader
 			);
 		}
 
-		if (isset($this->_config['cdn_domain']))
+		if ($this->cdn_domain != NULL))
 		{
 			$url = parse_url($result['ObjectURL']);
-			return array('url' => $this->_config['cdn_domain'] . $url['path']);
+			return array('url' => $this->cdn_domain . $url['path']);
 		}
 
 		return array('url' => $result['ObjectURL']);
-	}
-
-	private function decrypt($param)
-	{
-		$key = pack('H*', "79A0B95440D6302F5844225240DE85D68300C00EFAA3E117281B494D07681A35");
-		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-    	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-
-		$ciphertext_dec = base64_decode($param);
-	    $iv_dec = substr($ciphertext_dec, 0, $iv_size);
-	    $ciphertext_dec = substr($ciphertext_dec, $iv_size);
-
-	    return mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
 	}
 }
---
+++ README.md
@@ -0,0 +1,46 @@
+# CiiAWSUploader Class
+
+This class enables CiiMS to upload files to Amazon S3, isntead of uploading files to the uploads directory
+
+## How to Use
+
+To use this class, you need to make a configuration change to your ```protected/config/params.php``` file.
+
+```
+<?php return array(
+
+	[...]
+	'CiiMS' => array(
+		'upload' => array(
+			'class' => 'CiiAWSUploader',
+			'options' => array(
+				'bucket' => ''
+				'AWS_ACCESS_KEY' => '',
+				'AWS_SECRET_ACCESS_KEY' => '',
+				'cdn_domain' => 'https://cdn.example.com' // Optional
+			)
+		)
+	)
+	[...]
+);
+```
+
+# Options
+
+The following options are available for this class:
+
+__AWS_ACCESS_KEY__ (required)
+
+Your AWS Access Key
+
+__AWS_SECRET_ACCESS_KEY__ (required)
+
+Your AWS Secret Key
+
+__bucket__ (required)
+
+The bucket name
+
+__cdn_domain__ (optional)
+
+If you have a custom domain infront of AWS, you can specify it here. CiiMS will use this domain for the CDN urls instead of the default AWS domain
